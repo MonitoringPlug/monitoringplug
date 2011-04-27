5726f222b1562d598bcfb8abae07eb1287a461ac
Marius Rieder <marius.rieder@durchmesser.ch>
2011-04-27 13:50:54 +0000

    Switch to mp_snmp_table_query_new refs #18

--- snmp/check_qnap_disks.c
+++ snmp/check_qnap_disks.c
@@ -71,7 +71,7 @@ int main (int argc, char **argv) {
     /* OIDs to query */
     struct mp_snmp_query_cmd snmpcmd_table = {{1,3,6,1,4,1,24681,1,2,11}, 10, 0, (void *)&table_state};
 
-    status = mp_snmp_table_query(ss, &snmpcmd_table);
+    status = mp_snmp_table_query_new(ss, &snmpcmd_table, 7);
     if (status != STAT_SUCCESS) {
         char *string;
         snmp_error(ss, NULL, NULL, &string);
