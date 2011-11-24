e3f8c06556f65cbd9af54f300f0a044d46dd5b36
Marius Rieder <marius.rieder@durchmesser.ch>
2011-11-24 12:32:47 +0000

    Added pnp4nagios templates

---
+++ contrib/pnp/check_koji_hub.php
@@ -0,0 +1,64 @@
+<?php
+#
+# Monitoring Plugin - check_koji_hub.php
+#
+#
+# Copyright (C) 2011 Marius Rieder <marius.rieder@durchmesser.ch>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# $Id$
+#
+
+$opt[1] = "--vertical-label \"$UNIT[1]\" --title \"Response Times $hostname / $servicedesc\" ";
+$opt[2] = "--vertical-label \"Tasks\" --title \"Koji Tasks $hostname\" ";
+
+$ds_name[1] = "Response Time";
+$ds_name[2] = "Tasks";
+
+$ds_name[1] = "Response Time";
+$ds_name[2] = "Tasks";
+
+$def[1] =  "DEF:var1=$RRDFILE[1]:$DS[1]:AVERAGE " ;
+if ($WARN[1] != "") {
+    $def[1] .= "HRULE:$WARN[1]#FFFF00 ";
+}
+if ($CRIT[1] != "") {
+    $def[1] .= "HRULE:$CRIT[1]#FF0000 ";
+}
+$def[1] .= rrd::gradient("var1", "66CCFF", "0000ff", "$NAME[1]");
+$def[1] .= rrd::line1("var1", "666666");
+$def[1] .= rrd::gprint("var1", array("LAST","MAX","AVERAGE"), "%6.2lf $UNIT[1]");
+
+$def[2]  = rrd::def("free", $RRDFILE[1], $DS[2], "AVERAGE");
+$def[2] .= rrd::def("open", $RRDFILE[1], $DS[3], "AVERAGE");
+$def[2] .= rrd::def("closed", $RRDFILE[1], $DS[4], "AVERAGE");
+$def[2] .= rrd::def("canceled", $RRDFILE[1], $DS[5], "AVERAGE");
+$def[2] .= rrd::def("assigned", $RRDFILE[1], $DS[6], "AVERAGE");
+$def[2] .= rrd::def("faild", $RRDFILE[1], $DS[7], "AVERAGE");
+
+$def[2] .= rrd::area("free", "#0000DD", rrd::cut("Free",12));
+$def[2] .= rrd::gprint("free", array("LAST", "AVERAGE", "MAX"), "%6.2lf%s");
+$def[2] .= rrd::area("open", "#FF8000", rrd::cut("Open",12), true);
+$def[2] .= rrd::gprint("open", array("LAST", "AVERAGE", "MAX"), "%6.2lf%s");
+$def[2] .= rrd::area("closed", "#00FF00", rrd::cut("Closed",12), true);
+$def[2] .= rrd::gprint("closed", array("LAST", "AVERAGE", "MAX"), "%6.2lf%s");
+$def[2] .= rrd::area("canceled", "#FFFF00", rrd::cut("Canceled",12), true);
+$def[2] .= rrd::gprint("canceled", array("LAST", "AVERAGE", "MAX"), "%6.2lf%s");
+$def[2] .= rrd::area("faild", "#FF0000", rrd::cut("Faild",12), true);
+$def[2] .= rrd::gprint("faild", array("LAST", "AVERAGE", "MAX"), "%6.2lf%s");
+
+# vim: set ts=4 sw=4 et syn=php :
+?>
---
+++ contrib/pnp/check_nfs.php
@@ -0,0 +1,52 @@
+<?php
+#
+# Monitoring Plugin - check_smb_share.c
+#
+#
+# Copyright (C) 2011 Marius Rieder <marius.rieder@durchmesser.ch>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# $Id$
+#
+
+# Some Config
+$COLORS = array(
+    "tcp_v3"  => "#800000", "tcp_v4"  => "#FF0000",
+    "udp_v3"  => "#008000", "udp_v4"  => "#00FF00",
+    "tcp6_v3" => "#000080", "tcp6_v4" => "#0000FF",
+    "udp6_v3" => "#804000", "udp6_v4" => "#FF8000"
+);
+$NAMES = array(
+    "tcp_v3"  => "NFSv3 TCP",  "tcp_v4"  => "NFSv4 TCP",
+    "udp_v3"  => "NFSv3 UDP",  "udp_v4"  => "NFSv4 UDP",
+    "tcp6_v3" => "NFSv3 TCP6", "tcp6_v4" => "NFSv4 TCP6",
+    "udp6_v3" => "NFSv3 UDP6", "udp6_v4" => "NFSv4 UDP6"
+);
+
+$opt[1] = "--vertical-label \"$UNIT[1]\" --title \"Response Times $hostname / $servicedesc\" ";
+
+$ds_name[1] = "NFS Response Time";
+
+$def[1] = "";
+
+foreach ($this->DS as $KEY=>$VAL) {
+    $def[1] .= rrd::def("var_$KEY", $VAL['RRDFILE'], $VAL['DS'], "AVERAGE");
+    $def[1] .= rrd::line1("var_$KEY", $COLORS[$VAL['NAME']] , rrd::cut($NAMES[$VAL['NAME']],12));
+    $def[1].= rrd::gprint  ("var_$KEY", array("LAST","MAX","AVERAGE"), "%6.3lf %S".$VAL['UNIT']);
+}
+
+# vim: set ts=4 sw=4 et syn=php :
+?>
