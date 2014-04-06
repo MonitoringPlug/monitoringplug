7668b361d636f06ec86ac80ea5cc9b383120e0ba
Marius Rieder <marius.rieder@durchmesser.ch>
2014-04-06 15:37:48 +0200

    Add new library function set_ok,... and mp_exit
    This should help with plugins which accumulate state and output
    durign different states of the check.

--- lib/mp_common.c
+++ lib/mp_common.c
@@ -32,6 +32,10 @@
 
 unsigned int mp_timeout = 10;
 unsigned int mp_verbose = 0;
+int mp_state   = -1;
+char *mp_out_ok = NULL;
+char *mp_out_warning = NULL;
+char *mp_out_critical = NULL;
 
 void ok(const char *fmt, ...) {
     va_list ap;
@@ -45,6 +49,33 @@ void ok(const char *fmt, ...) {
     }
     printf("\n");
     exit(STATE_OK);
+}
+
+void set_ok(const char *fmt, ...) {
+    if (mp_state < STATE_OK)
+        mp_state = STATE_OK;
+
+    int len=0;
+    va_list ap;
+
+    // Estimate len
+    va_start(ap, fmt);
+    len = vsnprintf(NULL, 0, fmt, ap);
+    va_end(ap);
+
+    // Get buffer
+    if (mp_out_ok) {
+        mp_out_ok = mp_realloc(mp_out_ok, strlen(mp_out_ok) + len + 3);
+        strcpy(mp_out_ok+strlen(mp_out_ok), ", ");
+    } else {
+        mp_out_ok = mp_malloc(len);
+        *mp_out_ok = '\0';
+    }
+
+    // sprintf
+    va_start(ap, fmt);
+    vsnprintf(mp_out_ok+strlen(mp_out_ok), len+1, fmt, ap);
+    va_end(ap);
 }
 
 void warning(const char *fmt, ...) {
@@ -61,6 +92,33 @@ void warning(const char *fmt, ...) {
     exit(STATE_WARNING);
 }
 
+void set_warning(const char *fmt, ...) {
+    if (mp_state < STATE_WARNING)
+        mp_state = STATE_WARNING;
+
+    int len=0;
+    va_list ap;
+
+    // Estimate len
+    va_start(ap, fmt);
+    len = vsnprintf(NULL, 0, fmt, ap);
+    va_end(ap);
+
+    // Get buffer
+    if (mp_out_warning) {
+        mp_out_warning = mp_realloc(mp_out_warning, strlen(mp_out_warning) + len + 3);
+        strcpy(mp_out_warning+strlen(mp_out_warning), ", ");
+    } else {
+        mp_out_warning = mp_malloc(len);
+        *mp_out_warning = '\0';
+    }
+
+    // sprintf
+    va_start(ap, fmt);
+    vsnprintf(mp_out_warning+strlen(mp_out_warning), len+1, fmt, ap);
+    va_end(ap);
+}
+
 void critical(const char *fmt, ...) {
     va_list ap;
     printf("CRITICAL - ");
@@ -75,6 +133,33 @@ void critical(const char *fmt, ...) {
     exit(STATE_CRITICAL);
 }
 
+void set_critical(const char *fmt, ...) {
+    if (mp_state < STATE_CRITICAL)
+        mp_state = STATE_CRITICAL;
+
+    int len=0;            
+    va_list ap;                
+
+    // Estimate len
+    va_start(ap, fmt);         
+    len = vsnprintf(NULL, 0, fmt, ap);             
+    va_end(ap);       
+
+    // Get buffer   
+    if (mp_out_critical) {
+        mp_out_critical = mp_realloc(mp_out_critical, strlen(mp_out_critical) + len + 3);
+        strcpy(mp_out_critical+strlen(mp_out_critical), ", ");
+    } else {
+        mp_out_critical = mp_malloc(len);
+        *mp_out_critical = '\0';
+    }
+
+    // sprintf              
+    va_start(ap, fmt);       
+    vsnprintf(mp_out_critical+strlen(mp_out_critical), len+1, fmt, ap);
+    va_end(ap);   
+}
+
 void unknown(const char *fmt, ...) {
     va_list ap;
     printf("UNKNOWN - ");
@@ -87,6 +172,45 @@ void unknown(const char *fmt, ...) {
     }
     printf("\n");
     exit(STATE_UNKNOWN);
+}
+
+void mp_exit(const char *fmt, ...) {
+    va_list ap;
+    switch (mp_state) {
+        case -1:
+        case STATE_OK:
+            mp_state = STATE_OK;
+            printf("OK - ");
+            break;
+        case STATE_WARNING:
+            printf("WARNING - ");
+            break;
+        case STATE_CRITICAL:
+            printf("CRITICAL - ");
+            break;
+    }
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+    if (mp_out_critical) {
+        printf(" %s", mp_out_critical);
+    }
+    if (mp_out_warning) {
+        if (mp_state > STATE_WARNING)
+            printf(" Warning:");
+        printf(" %s", mp_out_warning);
+    }
+    if (mp_out_ok) {
+        if (mp_state > STATE_OK)
+            printf(" OK:");
+        printf(" %s", mp_out_ok);
+    }
+    if (mp_showperfdata && mp_perfdata) {
+        printf(" | %s", mp_perfdata);
+        free(mp_perfdata);
+    }
+    printf("\n");
+    exit(mp_state);
 }
 
 void usage(const char *fmt, ...) {
